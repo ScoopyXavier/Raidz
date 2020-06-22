31c96c1daa7e54055f28c8b0670d478788688aea
Charles <60372959+ScoopyXavier@users.noreply.github.com>
2020-06-22 13:43:30 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/scoops/SharedPrefManager.java
@@ -0,0 +1,80 @@
+package com.example.scoops;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class SharedPrefManager {
+
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private static final String SHARED_PREF_NAME = "mysharedpref12";
+    //private static final String KEY_FNAME = "Fname";
+    //private static final String KEY_LNAME = "Lname";
+    private static final String KEY_EMAIL = "Email";
+    private static final String KEY_ID = "ID";
+
+
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    public boolean userLogin(int id, String email){
+
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_ID, id);
+        editor.putString(KEY_EMAIL, email);
+        //editor.putString(KEY_FNAME, fname);
+        //editor.putString(KEY_LNAME, lname);
+
+
+        editor.apply();
+
+        return true;
+    }
+
+    public boolean isLoggedIn(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if(sharedPreferences.getString(KEY_EMAIL, null) != null){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean logout(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        return true;
+    }
+
+/*
+    public String getUserFname(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_FNAME, null);
+    }
+
+    public String getUserLname(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_LNAME, null);
+    }*/
+
+    public String getUserEmail(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_EMAIL, null);
+    }
+}
